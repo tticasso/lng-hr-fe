960a79ebabd055d0d7bb66dc1ef5764d1f15cd64
paickle77 <levanan.8b@gmail.com>
2026-02-12 13:42:09 +0700

    "SCRUM-20 FE | Integrate API to get info of actual Wor kDays ot and leave request"

--- src/apis/attendancesAPI.js
+++ src/apis/attendancesAPI.js
@@ -1,12 +1,15 @@
 import apiClient from "./apiClient";
 
 export const attendancesAPI = {
-   
-    getall: (month,year) => {
+
+    getall: (month, year) => {
         return apiClient.get(`/attendances/report?month=${month}&year=${year}`);
     },
-     getbyid: (month,year,id) => {
+    getbyid: (month, year, id) => {
         return apiClient.get(`/attendances/employee/${id}?month=${month}&year=${year}`);
     },
-   
+    getdatamoth: (month, year) => {
+        return apiClient.get(`/attendances/my-timesheet?month=${month}&year=${year}`);
+    },
+
 };
