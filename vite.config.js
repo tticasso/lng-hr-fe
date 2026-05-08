70ec2cff0f736a9d3321565a959e93e4a42d6a67
ngovanhieu <hieunvpzz1@gmail.com>
2026-05-09 01:32:23 +0700

    hotfix: load root

--- vite.config.js
+++ vite.config.js
@@ -4,27 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (!id.includes("node_modules")) return undefined;
-
-          if (
-            id.includes("node_modules/react/") ||
-            id.includes("node_modules/react-dom/") ||
-            id.includes("node_modules/react-router-dom/")
-          ) {
-            return "react-vendor";
-          }
-
-          if (id.includes("node_modules/@tiptap/")) {
-            return "editor-vendor";
-          }
-
-          return undefined;
-        },
-      },
-    },
-  },
 })
